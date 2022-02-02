99ccdc39fb8cd880c7663898fed776ae938a459d
Uzeda <gabrielsuzeda@gmail.com>
2022-02-02 13:14:14 +0000

    Update etapa2.sh

--- scripts/etapa2.sh
+++ scripts/etapa2.sh
@@ -21,9 +21,9 @@ emerge grub
 mkdir /boot/efi
 grub-install --efi-directory=/boot/efi
 #Instala e configura programas para viabilizar primeiro boot sem dor de cabeca
-emerge ccache eselect-repository networkmanager gentoo-kernel linux-firmware mold
+emerge ccache eselect-repository networkmanager gentoo-kernel-bin linux-firmware mold git
 mkdir /var/cache/ccache/
-echo 'max_size = 50.0G
+echo 'max_size = 30.0G
 umask = 002
 cache_dir_levels = 3
 compiler_check = %compiler% -v
@@ -34,6 +34,35 @@ mkdir -p /etc/portage/repos.conf
 grub-mkconfig -o /boot/grub/grub.cfg
 #Ativa alguns repositorios atualiza com o sistema e le as noticias
 eselect repository enable mv lto-overlay ppfeufer-gentoo-overlay
+#Configura o repo Portagepelo git
+echo "[DEFAULT]
+main-repo = gentoo
+[gentoo]
+location = /usr/portage
+sync-type = git
+sync-uri = https://github.com/gentoo/gentoo.git
+#sync-uri = https://github.com/gentoo-mirror/gentoo.git 
+#sync-uri = https://gitweb.gentoo.org/repo/sync/gentoo.git 
+auto-sync = yes
+ 
+#[DEFAULT]
+#main-repo = gentoo
+#
+#[gentoo]
+#location = /usr/portage
+#sync-type = rsync
+#sync-uri = rsync://rsync.gentoo.org/gentoo-portage
+#auto-sync = yes
+ 
+# for daily squashfs snapshots
+#sync-type = squashdelta
+#sync-uri = mirror://gentoo/../snapshots/squashfs" >  /etc/portage/repos.conf/gentoo.conf
+mkdir /usr/portage-rsync
+mv /usr/portage/* /usr/portage-rsync
+emerge --sync
+eselect news read
+mv /usr/portage-rsync/{distfiles,packages} /usr/portage/
+rm -rf /usr/portage-rsync
 emerge --sync 
 eselect news read
 # Habilita gerenciador de redes e configura seha
